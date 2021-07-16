ef37f783a050db38c99721e13f39467aa67518da
Arpit Batra <arpitbatra123@gmail.com>
2021-07-16 22:01:00 +0530

    stop using scss

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,2 @@
 mkdir -p dist
-
-# node-sass should be installed
-node-sass --output-style expanded --output dist/ src/
-
-cp -R src/ dist/
-
-# remove all scss files
-rm dist/styles/*.scss
-
 echo "done at $(date)"
